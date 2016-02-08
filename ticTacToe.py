24933282e7e6d8225ca167dca88915e2db7348a8
Elise Harper <eharper4@mail.valenciacollege.edu>
2016-02-07 21:25:31 -0500

    Tic Tac Toe Board

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,17 @@
+##Elise Harper Tic Tac Toe Board##
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
 def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -9,7 +22,16 @@ def printBoard(board):
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
-    
+
+    return((board['top-L'] == player and board['top-M'] == player and board['top-R'] == player) or #Check for winner across top
+           (board['mid-L'] == player and board['mid-M'] == player and board['mid-R'] == player) or #Check for winner across the middle
+           (board['low-L'] == player and board['low-M'] == player and board['low-R'] == player) or #Check for winner across bottom
+           (board['top-L'] == player and board['mid-L'] == player and board['low-L'] == player) or #Check for winner in left hand column
+           (board['top-M'] == player and board['mid-M'] == player and board['low-M'] == player) or #Check for winner in middle column
+           (board['top-R'] == player and board['mid-R'] == player and board['low-R'] == player) or #Check for winner in right hand column
+           (board['top-L'] == player and board['mid-M'] == player and board['low-R'] == player) or #Check for winner diagonally
+           (board['top-R'] == player and board['mid-M'] == player and board['low-L'] == player)) #Check for winner other diagonal direction
+             
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
     # to determine if the player stored in variable 'player' currently      #
@@ -26,23 +48,23 @@ def startGame(startingPlayer, board):
     # is happening. You do not need to modify any of the Python code        #
     #########################################################################
 
-    turn = startingPlayer
-    for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
-            print('X wins!')
-            break
-        elif ( checkWinner(board, 'O') ):
-            print('O wins!')
-            break
+    turn = startingPlayer  #Starting player picks where they want to put the first X
+    for i in range(9): #There are 9 spaces so this starts a loop that will run for each space
+        printBoard(board) #This prints the board after each turn
+        print('Turn for ' + turn + '. Move on which space?') #This asks each player where on the board they want to put their X or O
+        move = input() #This takes whatever the user enters and stores it in the variable 'move'
+        board[move] = turn  #This puts the users input on the actual board
+        if( checkWinner(board, 'X') ):  #This starts a loop that checks if the X has won the game
+            print('X wins!') #What the user sees if X has won the game
+            break  #Breaks the loop
+        elif ( checkWinner(board, 'O') ):  #This starts a loop that checks if O has won the game
+            print('O wins!')  #What the user sees if O has won the game
+            break    #Breaks the loop
     
-        if turn == 'X':
+        if turn == 'X':   #This changes turns from X to O
             turn = 'O'
-        else:
+        else:             #Switches back from O to X
             turn = 'X'
         
-    printBoard(board)
+    printBoard(board)   #The command that prints the board and makes the program start running
     
